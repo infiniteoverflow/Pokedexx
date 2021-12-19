19169d58bedc426aba3cb4ca1f60c84bee257106
Arvind Prime <singhamish27@gmail.com>
2021-12-20 02:03:50 +0530

    made Appbar on Homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -9,10 +9,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome Devs"
+      appBar: AppBar(
+        leading: Image.asset(
+          "assets/images/pokeball.png",
         ),
+        title: Image.asset(
+          "assets/images/pokemon_logo.png",
+          width: 190,
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // write your logic for search
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Welcome Devs"),
       ),
     );
   }
