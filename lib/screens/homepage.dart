404366d29e261fb191233ee40f1ba0a01e9c4cfe
Arvind Prime <singhamish27@gmail.com>
2021-12-24 22:53:29 +0530

    modified appbar with appropriate size of icons

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -10,17 +10,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Image.asset(
-          "assets/images/pokeball.png",
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 75,
+        leading: Container(
+          // width: 200,
+          child: Image.asset(
+            "assets/images/pokeball.png",
+            height: 100,
+            width: 150,
+            // scale: 5,
+            fit: BoxFit.fitHeight,
+          ),
         ),
-        title: Image.asset(
-          "assets/images/pokemon_logo.png",
-          width: 190,
+        title: Container(
+          child: Image.asset(
+            "assets/images/pokemon_logo.png",
+            width: 140,
+          ),
         ),
         centerTitle: true,
         actions: [
           IconButton(
-            icon: Icon(Icons.search),
+            icon: Icon(
+              Icons.search,
+              size: 40,
+              color: Colors.black,
+            ),
             onPressed: () {
               // write your logic for search
             },
