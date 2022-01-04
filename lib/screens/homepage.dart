2203284dccca2ad250a47fdec6bf9eeb874b4c7b
Arvind Prime <53316854+brainless-coder@users.noreply.github.com>
2022-01-04 09:50:10 +0530

    made Appbar on the Homepage (#7)
    * made Appbar on Homepage
    
    * modified appbar with appropriate size of icons

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -9,10 +9,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome Devs"
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 75,
+        leading: Container(
+          // width: 200,
+          child: Image.asset(
+            "assets/images/pokeball.png",
+            height: 100,
+            width: 150,
+            // scale: 5,
+            fit: BoxFit.fitHeight,
+          ),
         ),
+        title: Container(
+          child: Image.asset(
+            "assets/images/pokemon_logo.png",
+            width: 140,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              size: 40,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              // write your logic for search
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Welcome Devs"),
       ),
     );
   }
